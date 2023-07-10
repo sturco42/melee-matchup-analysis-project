7951851f8686364dbc5f66fab6162710d3aa3929
Sam <samturco42@gmail.com>
2023-07-09 21:10:41 -0700

    User model table made

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy_serializer import SerializerMixin
-
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 from config import db
 
 
@@ -11,6 +12,9 @@ class UserCharacter(db.Model, SerializerMixin):
     character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
     character = db.relationship('Character', back_populates='user_characters')
     user = db.relationship('User', back_populates='user_characters')
     
@@ -20,3 +24,22 @@ class UserCharacter(db.Model, SerializerMixin):
     def __repr__(self):
         return f'UserCharacter {self.id}, {self.character_id}, {self.user_id}'
 
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    user_characters = db.relationship('UserCharacter', back_populates='user', cascade='all')
+    character = association_proxy('user_characters', 'character')
+    
+    serialize_only = ('id', 'username', 'user_characters')
+    serialize_rules = ('user_characters.id', 'user_characters.user_id')
+    
+    def __repr__(self):
+        return f'User {self.username}'
