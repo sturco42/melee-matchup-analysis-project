81d251b652c83f77abd3308438379709653641b6
Sam <samturco42@gmail.com>
2023-07-19 00:33:46 -0700

    taking out unecessary import in notbooktitle

--- client/src/components/NotebookTitle.js
+++ client/src/components/NotebookTitle.js
@@ -1,5 +1,5 @@
 // imports
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 
 const NotebookTitle = ( { id, character, removeNotebook, onClick} ) => {
